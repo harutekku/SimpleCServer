dc19c3c4c7c8f02f5e918fffeacba3652fb0109e
PunishedBigSnek <89872420+PunishedBigSnek@users.noreply.github.com>
2021-09-01 00:03:06 +0200

    Add files via upload

--- src/diagnostics.c
+++ src/diagnostics.c
@@ -1,7 +1,7 @@
 #include "../include/diagnostics.h"
 
 void GetTime(char* buffer, size_t size) {
-	time_t     currentTime = time(NULL);
-	strftime(buffer, size, "%X", localtime(&currentTime));
+    time_t     currentTime = time(NULL);
+    strftime(buffer, size, "%X", localtime(&currentTime));
 }
 
--- src/main.c
+++ src/main.c
@@ -24,16 +24,16 @@ int main(void) {
     struct addrinfo*        i = NULL;
     char                    myIP[INET6_ADDRSTRLEN] = { 0 };
     int                     ec = 0;
-	const size_t            timeSize = 20;
-	char                    time[timeSize] = { 0 };
+    const size_t            timeSize = 20;
+    char                    time[timeSize] = { 0 };
 
     // Their data
     struct sockaddr_storage theirAddress;
     socklen_t               theirAddressSize = sizeof(struct sockaddr_storage);
     int                     theirFd = -1;
     char                    theirIP[INET6_ADDRSTRLEN];
-	char                    theirMessage[SIZE] = { 0 };
-	
+    char                    theirMessage[SIZE] = { 0 };
+    
     //------------------------------------------------------------------------------------------
     // Prepare hints
     memset(&hints, 0, sizeof(struct addrinfo));
@@ -44,30 +44,30 @@ int main(void) {
     //------------------------------------------------------------------------------------------
     // System calls
     if ((ec = getaddrinfo(NULL, "7590", &hints, &res)) != 0) {
-		GetTime(time, timeSize);
+        GetTime(time, timeSize);
         fprintf(stderr, "DEBUG: [%s] => getaddrinfo(): %s\n", time, gai_strerror(ec));
         return EXIT_FAILURE;
     }
 
     for (i = res; i != NULL; i = i->ai_next) {
         if ((fd = socket(i->ai_family, i->ai_socktype, i->ai_protocol)) == -1) {
-			GetTime(time, timeSize);
-			fprintf(stderr, "DEBUG: [%s] => socket(): %s\n", time, strerror(errno));
+            GetTime(time, timeSize);
+            fprintf(stderr, "DEBUG: [%s] => socket(): %s\n", time, strerror(errno));
             continue;
         } else if (bind(fd, i->ai_addr, i->ai_addrlen) == -1) {
             close(fd);
-			GetTime(time, timeSize);
-			fprintf(stderr, "DEBUG: [%s] => bind(): %s\n", time, strerror(errno));
+            GetTime(time, timeSize);
+            fprintf(stderr, "DEBUG: [%s] => bind(): %s\n", time, strerror(errno));
             continue;
         } else {
             if (i->ai_family == AF_INET) {
                 inet_ntop(AF_INET, &((struct sockaddr_in*)&i->ai_addr)->sin_addr, myIP, INET6_ADDRSTRLEN);
-				GetTime(time, timeSize);
-				fprintf(stdout, "DEBUG: [%s] => Server active on IP: %s\n", time, myIP);
+                GetTime(time, timeSize);
+                fprintf(stdout, "DEBUG: [%s] => Server active on IP: %s\n", time, myIP);
             } else {
                 inet_ntop(AF_INET6, &((struct sockaddr_in6*)&i->ai_addr)->sin6_addr, myIP, INET6_ADDRSTRLEN);
-				GetTime(time, timeSize);
-				fprintf(stdout, "DEBUG: [%s] => Server active on IP: %s\n", time, myIP);
+                GetTime(time, timeSize);
+                fprintf(stdout, "DEBUG: [%s] => Server active on IP: %s\n", time, myIP);
             }
             break;
         }
@@ -76,14 +76,14 @@ int main(void) {
     freeaddrinfo(res);
 
     if (!i) {
-		GetTime(time, timeSize);
-		fprintf(stderr, "DEBUG: [%s] => Unable to launch server\n", time);
+        GetTime(time, timeSize);
+        fprintf(stderr, "DEBUG: [%s] => Unable to launch server\n", time);
         return EXIT_FAILURE;
     }
 
     if (listen(fd, BACKLOG) == -1) {
-		GetTime(time, timeSize);
-		fprintf(stderr, "DEBUG: [%s] => listen():%s\n", time, strerror(errno));
+        GetTime(time, timeSize);
+        fprintf(stderr, "DEBUG: [%s] => listen():%s\n", time, strerror(errno));
         return EXIT_FAILURE;
     }
 
@@ -92,25 +92,25 @@ int main(void) {
         theirFd = accept(fd, (struct sockaddr*)&theirAddress, &theirAddressSize);
 
         if (theirFd == -1) {
-			GetTime(time, timeSize);
-			fprintf(stderr, "DEBUG: [%s] => accept(): %s\n", time, strerror(errno));
+            GetTime(time, timeSize);
+            fprintf(stderr, "DEBUG: [%s] => accept(): %s\n", time, strerror(errno));
             continue;
         }
 
         if (theirAddress.ss_family == AF_INET) {
             inet_ntop(AF_INET, &((struct sockaddr_in*)&theirAddress)->sin_addr, theirIP, INET6_ADDRSTRLEN);
-			GetTime(time, timeSize);
-			fprintf(stderr, "DEBUG: [%s] => IP %s connected\n", time, theirIP);
+            GetTime(time, timeSize);
+            fprintf(stderr, "DEBUG: [%s] => IP %s connected\n", time, theirIP);
         } else {
             inet_ntop(AF_INET6, &((struct sockaddr_in6*)&theirAddress)->sin6_addr, theirIP, INET6_ADDRSTRLEN);
-			GetTime(time, timeSize);
-			fprintf(stderr, "DEBUG: [%s] => IP %s connected\n", time, theirIP);
+            GetTime(time, timeSize);
+            fprintf(stderr, "DEBUG: [%s] => IP %s connected\n", time, theirIP);
         }
-		size_t bytes = recv(theirFd, theirMessage, SIZE, 0);
-		theirMessage[bytes] = '\0';
+        size_t bytes = recv(theirFd, theirMessage, SIZE, 0);
+        theirMessage[bytes] = '\0';
 
-		GetTime(time, timeSize);
-		fprintf(stderr, "DEBUG: [%s] => IP %s sent [%zu bytes]:\n%s\n", time, theirIP, bytes, theirMessage);
+        GetTime(time, timeSize);
+        fprintf(stderr, "DEBUG: [%s] => IP %s sent [%zu bytes]:\n%s\n", time, theirIP, bytes, theirMessage);
 
         close(theirFd);
     }
